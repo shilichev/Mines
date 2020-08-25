489be4dc3e3252d1de541ff6c15c123b0a5b52e5
shilichev <52910393+shilichev@users.noreply.github.com>
2020-08-25 16:50:16 +0300

    without timer

--- main.js
+++ main.js
@@ -1,168 +1,266 @@
-id = [];
-let massive = [];
-let horizontal = 9;
-let vertical = 9;
-let mines = 10;
-let classOfNumbers = "";
+console.clear();
 
-const randomMines = () => {
-  for (i = 0; i < mines; i++) {
-    x = Math.random() * horizontal;
-    y = Math.random() * vertical;
-    massive.push(`${Math.floor(x)}` + `${Math.floor(y)}`);
-  }
-  console.log(massive);
+let size = 12;
+let bombFrequency = 0.2;
+let tileSize = 50;
+let firstClick = true;
+let sec = 0;
+
+const board = document.querySelectorAll(".board")[0];
+let tiles;
+let boardSize;
+
+const restartBtn = document.querySelectorAll(".minesweeper-btn")[0];
+const endscreen = document.querySelectorAll(".endscreen")[0];
+
+const boardSizeBtn = document.querySelectorAll(".difficulty");
+const tileSizeBtn = document.getElementById("tileSize");
+const difficultyBtns = document.querySelectorAll(".difficulty");
+
+let bombs = [];
+let numbers = [];
+let numberColors = [
+  "#3498db",
+  "#2ecc71",
+  "#e74c3c",
+  "#9b59b6",
+  "#f1c40f",
+  "#1abc9c",
+  "#34495e",
+  "#7f8c8d",
+];
+let endscreenContent = { win: "<span>YOU WON!</span>", loose: "Try again" };
+
+let gameOver = false;
+
+const clear = () => {
+  gameOver = false;
+  bombs = [];
+  numbers = [];
+  endscreen.innerHTML = "";
+  endscreen.classList.remove("show");
+  tiles.forEach((tile) => {
+    tile.remove();
+  });
+  sec = 0;
+  setup();
 };
-randomMines();
-console.log(massive);
 
-const showResult = (x, y) => {
-  $(".playZone").append(
-    `<div class= " size back-result-0 closed" id=${x}${y}>
-    </div>`
+const setup = () => {
+  for (let i = 0; i < Math.pow(size, 2); i++) {
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+    board.appendChild(tile);
+  }
+  tiles = document.querySelectorAll(".tile");
+  boardSize = Math.sqrt(tiles.length);
+  board.style.width = boardSize * tileSize + "px";
+
+  document.documentElement.style.setProperty("--tileSize", `${tileSize}px`);
+  document.documentElement.style.setProperty(
+    "--boardSize",
+    `${boardSize * tileSize}px`
   );
-};
 
-const getMassive = () => {
-  for (i = 0; i < horizontal; i++) {
-    id[i] = [];
-    for (j = 0; j < vertical; j++) {
-      id[i][j] = 0;
-      showResult(i, j);
-      clickFunction(i, j);
+  let x = 0;
+  let y = 0;
+  tiles.forEach((tile, i) => {
+    tile.setAttribute("data-tile", `${x},${y}`);
+
+    let random_boolean = Math.random() < bombFrequency;
+    if (random_boolean) {
+      bombs.push(`${x},${y}`);
+      if (x > 0) numbers.push(`${x - 1},${y}`);
+      if (x < boardSize - 1) numbers.push(`${x + 1},${y}`);
+      if (y > 0) numbers.push(`${x},${y - 1}`);
+      if (y < boardSize - 1) numbers.push(`${x},${y + 1}`);
+
+      if (x > 0 && y > 0) numbers.push(`${x - 1},${y - 1}`);
+      if (x < boardSize - 1 && y < boardSize - 1)
+        numbers.push(`${x + 1},${y + 1}`);
+
+      if (y > 0 && x < boardSize - 1) numbers.push(`${x + 1},${y - 1}`);
+      if (x > 0 && y < boardSize - 1) numbers.push(`${x - 1},${y + 1}`);
     }
-  }
-  countNumbers(id);
-};
 
-const showBomb = () => {
-  massive.forEach((id) => {
-    $(`#${id}`).addClass(`back-result-mine`);
-  });
-};
+    x++;
+    if (x >= boardSize) {
+      x = 0;
+      y++;
+    }
 
-const checkMassive = (id) => {
-  for (i = 0; i < horizontal; i++) {
-    for (j = 0; j < vertical; j++) {
-      if (id[i][j] === 1) {
-        $(`#${i}${j}`).addClass(`back-result-1`);
-        $(`#${i}${j}`).removeClass("back-result-0");
-      } else if (id[i][j] === 2) {
-        $(`#${i}${j}`).addClass(`back-result-2`);
-        $(`#${i}${j}`).removeClass("back-result-0");
-      } else if (id[i][j] === 3) {
-        $(`#${i}${j}`).addClass(`back-result-3`);
-        $(`#${i}${j}`).removeClass("back-result-0");
-      } else if (id[i][j] === 4) {
-        $(`#${i}${j}`).addClass(`back-result-4`);
-        $(`#${i}${j}`).removeClass("back-result-0");
-      } else if (id[i][j] === 5) {
-        $(`#${i}${j}`).addClass(`back-result-5`);
-        $(`#${i}${j}`).removeClass("back-result-0");
-      } else if (id[i][j] === 6) {
-        $(`#${i}${j}`).addClass(`back-result-6`);
-        $(`#${i}${j}`).removeClass("back-result-0");
-      } else if (id[i][j] === 7) {
-        $(`#${i}${j}`).addClass(`back-result-7`);
-        $(`#${i}${j}`).removeClass("back-result-0");
+    tile.oncontextmenu = function (e) {
+      e.preventDefault();
+      flag(tile);
+    };
+
+    tile.addEventListener("click", function (e) {
+      clickTile(tile);
+      if (firstClick) {
+        init();
+        firstClick = false;
       }
-    }
-  }
+    });
+  });
+
+  numbers.forEach((num) => {
+    let coords = num.split(",");
+    let tile = document.querySelectorAll(
+      `[data-tile="${parseInt(coords[0])},${parseInt(coords[1])}"]`
+    )[0];
+    let dataNum = parseInt(tile.getAttribute("data-num"));
+    if (!dataNum) dataNum = 0;
+    tile.setAttribute("data-num", dataNum + 1);
+  });
 };
 
-const countNumbers = (id) => {
-  massive.forEach((bombId) => {
-    let x = parseInt(`${bombId}`.substring(0, 1));
-    let y = parseInt(`${bombId}`.substring(1, 3));
-    if (x === 0 && y === 0) {
-      id[x][y + 1]++;
-      id[x + 1][y]++;
-      id[x + 1][y + 1]++;
-    } else if (x === 0 && y === vertical - 1) {
-      id[x][y - 1]++;
-      id[x + 1][y - 1]++;
-      id[x + 1][y]++;
-    } else if (x === horizontal - 1 && y === 0) {
-      id[x - 1][y]++;
-      id[x - 1][y + 1]++;
-      id[x][y + 1]++;
-    } else if (x === horizontal - 1 && y === vertical - 1) {
-      id[x - 1][y - 1]++;
-      id[x - 1][y]++;
-      id[x][y - 1]++;
-    } else if (y === 0) {
-      id[x - 1][y + 1]++;
-      id[x - 1][y]++;
-      id[x][y + 1]++;
-      id[x + 1][y]++;
-      id[x + 1][y + 1]++;
-    } else if (x === 0) {
-      id[x][y - 1]++;
-      id[x][y + 1]++;
-      id[x + 1][y - 1]++;
-      id[x + 1][y]++;
-      id[x + 1][y + 1]++;
-    } else if (y === vertical - 1) {
-      id[x - 1][y - 1]++;
-      id[x - 1][y]++;
-      id[x][y - 1]++;
-      id[x + 1][y]++;
-      id[x + 1][y - 1]++;
-    } else if (x === horizontal - 1) {
-      id[x - 1][y + 1]++;
-      id[x - 1][y - 1]++;
-      id[x][y + 1]++;
-      id[x - 1][y]++;
-      id[x][y - 1]++;
+const flag = (tile) => {
+  if (gameOver) return;
+  if (!tile.classList.contains("tile--checked")) {
+    if (!tile.classList.contains("tile--flagged")) {
+      tile.innerHTML = "x";
+      tile.classList.add("tile--flagged");
     } else {
-      id[x - 1][y - 1]++;
-      id[x - 1][y]++;
-      id[x - 1][y + 1]++;
-      id[x][y - 1]++;
-      id[x][y + 1]++;
-      id[x + 1][y - 1]++;
-      id[x + 1][y]++;
-      id[x + 1][y + 1]++;
+      tile.innerHTML = "";
+      tile.classList.remove("tile--flagged");
     }
-  });
-  checkMassive(id);
-  return id;
-};
-// const showZeroResult = (x, y) => {
-//   showZero(x - 1, y - 1);
-//   showZero(x + 1, y + 1);
-//   showZero(x + 1, y - 1);
-//   showZero(x - 1, y + 1);
-//   showZero(x, y - 1);
-//   showZero(x, y + 1);
-//   showZero(x + 1, y);
-//   showZero(x - 1, y);
-// };
-const checkZero = (classOfNumbers, x, y) => {
-  if (classOfNumbers === "back-result-0") {
-    $(`#${x}${y}`).removeClass("closed");
-  
-    getClassOfNumbers(x, y);
-    
-    console.log(0);
   }
 };
 
-const getClassOfNumbers = (x, y) => {
-  let str = $(`#${x}${y}`).attr("class");
-  classOfNumbers = str.split(" ").slice(1).join(" ");
-  checkZero(classOfNumbers, x, y);
-};
-const clickFunction = (x, y) => {
-  $(`#${x}${y}`).click(function () {
-    $(`#${x}${y}`).removeClass("closed");
-    getClassOfNumbers(x, y);
+const clickTile = (tile) => {
+  if (gameOver) return;
+  if (
+    tile.classList.contains("tile--checked") ||
+    tile.classList.contains("tile--flagged")
+  )
+    return;
+  let coordinate = tile.getAttribute("data-tile");
+  if (bombs.includes(coordinate)) {
+    endGame(tile);
+  } else {
+    let num = tile.getAttribute("data-num");
+    if (num != null) {
+      tile.classList.add("tile--checked");
+      tile.innerHTML = num;
+      tile.style.color = numberColors[num - 1];
+      setTimeout(() => {
+        checkVictory();
+      }, 100);
+      return;
+    }
 
-    // if (newStr === "back-result-0") {
-    //   showZeroResult(x, y);
-    // }
+    checkTile(tile, coordinate);
+  }
+  tile.classList.add("tile--checked");
+};
+
+const checkTile = (tile, coordinate) => {
+  console.log("вњ”");
+  let coords = coordinate.split(",");
+  let x = parseInt(coords[0]);
+  let y = parseInt(coords[1]);
+
+  setTimeout(() => {
+    if (x > 0) {
+      let targetW = document.querySelectorAll(`[data-tile="${x - 1},${y}"`)[0];
+      clickTile(targetW, `${x - 1},${y}`);
+    }
+    if (x < boardSize - 1) {
+      let targetE = document.querySelectorAll(`[data-tile="${x + 1},${y}"`)[0];
+      clickTile(targetE, `${x + 1},${y}`);
+    }
+    if (y > 0) {
+      let targetN = document.querySelectorAll(`[data-tile="${x},${y - 1}"]`)[0];
+      clickTile(targetN, `${x},${y - 1}`);
+    }
+    if (y < boardSize - 1) {
+      let targetS = document.querySelectorAll(`[data-tile="${x},${y + 1}"]`)[0];
+      clickTile(targetS, `${x},${y + 1}`);
+    }
+
+    if (x > 0 && y > 0) {
+      let targetNW = document.querySelectorAll(
+        `[data-tile="${x - 1},${y - 1}"`
+      )[0];
+      clickTile(targetNW, `${x - 1},${y - 1}`);
+    }
+    if (x < boardSize - 1 && y < boardSize - 1) {
+      let targetSE = document.querySelectorAll(
+        `[data-tile="${x + 1},${y + 1}"`
+      )[0];
+      clickTile(targetSE, `${x + 1},${y + 1}`);
+    }
+
+    if (y > 0 && x < boardSize - 1) {
+      let targetNE = document.querySelectorAll(
+        `[data-tile="${x + 1},${y - 1}"]`
+      )[0];
+      clickTile(targetNE, `${x + 1},${y - 1}`);
+    }
+    if (x > 0 && y < boardSize - 1) {
+      let targetSW = document.querySelectorAll(
+        `[data-tile="${x - 1},${y + 1}"`
+      )[0];
+      clickTile(targetSW, `${x - 1},${y + 1}`);
+    }
+  }, 10);
+};
+
+const endGame = (tile) => {
+  console.log("Booom! Game over.");
+  endscreen.innerHTML = endscreenContent.loose;
+  endscreen.classList.add("show");
+  gameOver = true;
+  tiles.forEach((tile) => {
+    let coordinate = tile.getAttribute("data-tile");
+    if (bombs.includes(coordinate)) {
+      tile.classList.remove("tile--flagged");
+      tile.classList.add("tile--checked", "tile--bomb");
+      tile.innerHTML = "*";
+    }
   });
 };
 
-getMassive();
-showBomb();
+const checkVictory = () => {
+  let win = true;
+  tiles.forEach((tile) => {
+    let coordinate = tile.getAttribute("data-tile");
+    if (
+      !tile.classList.contains("tile--checked") &&
+      !bombs.includes(coordinate)
+    )
+      win = false;
+  });
+  if (win) {
+    endscreen.innerHTML = endscreenContent.win;
+    endscreen.classList.add("show");
+    gameOver = true;
+  }
+};
+
+setup();
+
+restartBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  clear();
+});
+
+difficultyBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    console.log(this.value);
+    bombFrequency = this.value / 70;
+    size = this.value;
+    tileSize = 70 - size * 2;
+
+    clear();
+  });
+});
+function init() {
+  if (!(gameOver)) {
+    setInterval(tick, 1000);
+  }
+}
+function tick() {
+  sec++;
+  document.getElementById("timer").childNodes[0].nodeValue = sec;
+}
